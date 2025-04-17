ce0650d395245a28b26de6d628cdf395acb27d93
BilalAulakh <bb807562@gmail.com>
2025-04-17 23:53:56 +0500

    jasdi

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res, next) => {
   res.send("hello word");
 });
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
